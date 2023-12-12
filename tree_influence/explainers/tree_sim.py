51b40f2faa3898f18ffdf1d67b590aca4bf0b354
saptak-narula <64187611+saptak-narula@users.noreply.github.com>
2023-12-12 14:49:13 +0530

    Update tree_sim.py

--- tree_influence/explainers/tree_sim.py
+++ tree_influence/explainers/tree_sim.py
@@ -107,16 +107,17 @@ class TreeSim(Explainer):
                 for a, b in zip(top_ctrs_X_test_trees[test_ctr], top_ctrs_X_test_leafs[test_ctr]):
                     #train_elems.extend(train_point_dict.get((a,b)))
                     train_elems.update(train_point_dict.get((a,b)))
+                return(train_elems)
                 #train_elems = list(set(train_elems))
                 #sim = np.dot(np.equal(self.X_train_[train_elems,:,0], X_test_[test_ctr,:,0]), X_test_[test_ctr,:,1])
                 #sgn = np.equal(self.y_train_[train_elems], y[test_ctr])*2.0 - 1.0
                 #influence[train_elems, test_ctr] = sim * sgn
-            run_simy_loop()
-            def compute_simy():
+            train_elems = run_simy_loop()
+            def compute_simy(train_elems):
                 sim = np.dot(np.equal(self.X_train_[list(train_elems),:,0], X_test_[test_ctr,:,0]), X_test_[test_ctr,:,1])
                 sgn = np.equal(self.y_train_[list(train_elems)], y[test_ctr])*2.0 - 1.0
+                influence[list(train_elems), test_ctr] = sim * sgn
             compute_simy()
-            influence[list(train_elems), test_ctr] = sim * sgn
        
         return influence
 
